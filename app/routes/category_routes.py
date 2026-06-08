c884ebd8a67042f60bb78380511642e7735dfb5d
XX-chan <chanx681@gmail.com>
2026-06-08 16:42:34 +0800

    feat(routes): 完成 category 所有路由功能

---
+++ app/routes/category_routes.py
@@ -0,0 +1,59 @@
+from flask import Blueprint,session,request,jsonify
+from app.services import add_categroy,get_categories_dict,delete_category
+
+cg_bp=Blueprint("category",__name__)
+
+#新增分类
+@cg_bp.route("/add_cg",methods="[POST]")
+def add_cg():
+    user_id=session.get("userid")
+    name=request.get_json("category_name")
+    type=request.get_json("category_type")
+    category=add_categroy(user_id=user_id,name=name,type=type)
+    if category:
+        return jsonify({
+            "success": True,
+            "data":category,
+            "message":"添加分类成功"
+        }),200
+    return jsonify({
+            "success": False,
+            "data":None,
+            "message":"添加分类失败"
+        }),500
+
+
+#获取不同type的分类明细
+@cg_bp.route("/get_cgs/String:cg_type")
+def get_categories(cg_type):
+    user_id=session.get("user_id")
+    categories=get_categories_dict(user_id=user_id,category_type=cg_type)
+    if categories:
+        return jsonify({
+            "success": True,
+            "data":categories,
+            "message":"请求成功"
+        }),200
+    return jsonify({
+            "success": False,
+            "data":None,
+            "message":"请求出错"
+        }),500
+
+
+#删除分类
+@cg_bp.route("/delete_cg/<int:cg_id",methods=["DELETE"])
+def delete_cg(cg_id):
+    user_id=session.get("user_id")
+    result=delete_category(user_id=user_id,category_id=cg_id)
+    if result:
+        return jsonify({
+            "success": True,
+            "data":None,
+            "message":"删除成功"
+        }),200
+    return jsonify({
+            "success": False,
+            "data":None,
+            "message":"删除失败"
+        }),500
