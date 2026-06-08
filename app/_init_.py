c0a71ee78e023da704c63a2f14a37f70bee9c8da
XX-chan <chanx681@gmail.com>
2026-06-08 18:03:45 +0800

    chore(app): 配置应用工厂模式
    - 创建 db 实例
    - 关联 app 和 db
    - 注册蓝图

--- app/_init_.py
+++ app/_init_.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from config import get_config
+from sqlalchemy import SQLALchemy
+
+#创建db实例
+db = SQLALchemy()
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_object(get_config())
 
+    # 将db与app关联
+    db.init_app(app)
+
+
+    from routes import user_bp,ts_bp,cg_bp
     #注册蓝图
     app.register_blueprint(user_bp)
-    app.register_blueprint(transaction_bp)
-    app.register_blueprint(category_bp)
+    app.register_blueprint(ts_bp)
+    app.register_blueprint(cg_bp)
 
     return app
